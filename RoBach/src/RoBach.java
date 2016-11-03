c2867d49e669a1b577c79df23829c8752589b1e7
StevenGonzalez <gonzalez.steven.m@gmail.com>
2016-11-02 23:45:27 -0500

    Update RoBach.java

--- RoBach/src/RoBach.java
+++ RoBach/src/RoBach.java
@@ -4,9 +4,9 @@
  * 
  * Program: RoBach
  * Description: When this program is started, it will ask the user for a musical key and the beats per minute they would like
- * 				the program to play at. Once the user inputs this information, the program will start playing random notes
- * 				and chords at random lengths in the scale chosen by the user. Note that a third party program that emulates
- * 				an instrument playing by keyboard is required for this to work.
+ * 		the program to play at. Once the user inputs this information, the program will start playing random notes
+ * 		and chords at random lengths in the scale chosen by the user. Note that a third party program that emulates
+ * 		an instrument playing by keyboard is required for this to work.
  */
 
 
